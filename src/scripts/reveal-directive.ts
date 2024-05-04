a370e168d6de178e4f41bcd747d88ac6c91b602e
Max Muehlbauer <MaxMuehlbauer@users.noreply.github.com>
2024-05-05 00:36:46 +0200

    fix typing issue

--- src/scripts/reveal-directive.ts
+++ src/scripts/reveal-directive.ts
@@ -12,14 +12,14 @@ export const revealDirective: DirectiveCallback = (el, { expression }, { evaluat
     function reveal(newContent: string, delay: number = 0) {
         let currentIndex = 0;
 
-        interval = setInterval(() => {
+        interval = window.setInterval(() => {
             if (currentIndex < delay) {
                 currentIndex++;
                 return;
             }
 
             if (currentIndex - delay > newContent.length - 1) {
-                clearInterval(interval);
+                window.clearInterval(interval);
             }
 
             el.textContent = newContent.substring(0, currentIndex - delay + 1) + (oldContent as string).substring(currentIndex - delay + 1);
